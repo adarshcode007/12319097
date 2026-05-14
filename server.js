9c84c0167f4d6dc8f09b137c5e0928a7d76723ca
adarshcode007 <adarsh19pandey@gmail.com>
2026-05-14 17:02:43 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
